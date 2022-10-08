7753ac15a6ae8787cf2cad35f271c463b3cb8973
Matej Zidek <zidek.matt@gmail.com>
2022-10-08 21:53:04 +0200

    refactor: access MaterialColor safely thru shades getter

---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NepanikarColors {
+  NepanikarColors._();
+
+  static final MaterialColor primarySwatch = MaterialColor(background.value, _primarySwatch);
+  static const primary = Color(0xff280446);
+  static const background = Color(0xff491475);
+  static const secondary = Color(0xff4EA3AD);
+  static const error = Color(0xffD86C66);
+  static const success = Color(0xff6FD866);
+
+  static const _primarySwatch = <int, Color>{
+    50: Color(0xffFAF4FF), //10%
+    100: Color(0xffFAF4FF), //20%
+    200: Color(0xffE2D2EF), //30%
+    300: Color(0xffE2D2EF), //40%
+    400: Color(0xffC1AAD4), //50%
+    500: Color(0xffAF87C6), //60%
+    600: Color(0xffC090FC), //70%
+    700: Color(0xff955EB6), //80%
+    800: background, //90%
+    900: Color(0xff280446), //100%
+  };
+}
--- lib/app/theme/theme.dart
+++ lib/app/theme/theme.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:nepanikar/app/theme/colors.dart';
 import 'package:nepanikar/app/theme/sizes.dart';
 
 class NepanikarTheme {
   NepanikarTheme._();
 
   static final ThemeData themeData = ThemeData(
-    primaryColor: primary,
+    primaryColor: NepanikarColors.primary,
     scaffoldBackgroundColor: const Color(0xffFBF6FF),
     colorScheme: const ColorScheme.light().copyWith(
-      primary: primary,
-      secondary: secondary,
+      primary: NepanikarColors.primary,
+      secondary: NepanikarColors.secondary,
       onSecondary: Colors.white,
-      error: error,
+      error: NepanikarColors.error,
     ),
-    errorColor: error,
-    primarySwatch: MaterialColor(
-      background.value,
-      primarySwatch,
-    ),
+    errorColor: NepanikarColors.error,
+    primarySwatch: NepanikarColors.primarySwatch,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: _buttonStyle.copyWith(
         backgroundColor: MaterialStateProperty.resolveWith<Color?>(
           (states) {
-            if (states.contains(MaterialState.disabled)) return NepanikarTheme.primarySwatch[500]!;
-            return NepanikarTheme.background;
+            if (states.contains(MaterialState.disabled)) return NepanikarColors.primarySwatch[500]!;
+            return NepanikarColors.background;
           },
         ),
         foregroundColor: MaterialStateProperty.all<Color?>(Colors.white),
@@ -37,43 +35,24 @@ class NepanikarTheme {
         backgroundColor: MaterialStateProperty.all<Color?>(Colors.white),
         foregroundColor: MaterialStateProperty.resolveWith<Color?>(
           (states) {
-            if (states.contains(MaterialState.disabled)) return NepanikarTheme.primarySwatch[500]!;
-            return NepanikarTheme.primary;
+            if (states.contains(MaterialState.disabled)) return NepanikarColors.primarySwatch[500]!;
+            return NepanikarColors.primary;
           },
         ),
         textStyle: MaterialStateProperty.all<TextStyle>(
-          const TextStyle(color: primary, fontWeight: FontWeight.w900),
+          const TextStyle(color: NepanikarColors.primary, fontWeight: FontWeight.w900),
         ),
         side: MaterialStateProperty.resolveWith<BorderSide?>(
           (states) {
             if (states.contains(MaterialState.disabled)) {
-              return BorderSide(color: NepanikarTheme.primarySwatch[500]!, width: 2.0);
+              return BorderSide(color: NepanikarColors.primarySwatch[500]!, width: 2.0);
             }
-            return const BorderSide(color: NepanikarTheme.background, width: 2.0);
+            return const BorderSide(color: NepanikarColors.background, width: 2.0);
           },
         ),
       ),
     ),
   );
-
-  // TODO: refactor to MaterialColor for safe way of getting shade colors
-  static const primarySwatch = <int, Color>{
-    50: Color(0xffFAF4FF), //10%
-    100: Color(0xffFAF4FF), //20%
-    200: Color(0xffE2D2EF), //30%
-    300: Color(0xffE2D2EF), //40%
-    400: Color(0xffC1AAD4), //50%
-    500: Color(0xffAF87C6), //60%
-    600: Color(0xffC090FC), //70%
-    700: Color(0xff955EB6), //80%
-    800: background, //90%
-    900: Color(0xff280446), //100%
-  };
-  static const primary = Color(0xff280446);
-  static const background = Color(0xff491475);
-  static const secondary = Color(0xff4EA3AD);
-  static const error = Color(0xffD86C66);
-  static const success = Color(0xff6FD866);
 }
 
 final _buttonStyle = ButtonStyle(
