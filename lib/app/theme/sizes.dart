c88b88181b5e93aa3df9e9a34a95ee982df6a918
Matej Zidek <zidek.matt@gmail.com>
2022-10-08 21:26:12 +0200

    feat: design system - buttons

---
+++ lib/app/theme/sizes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class NepanikarSizes {
+  const NepanikarSizes._();
+
+  static const buttonSize = Size(290, 52);
+}
--- lib/screens/playground_screen.dart
+++ lib/screens/playground_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nepanikar/app/generated/assets.gen.dart';
 import 'package:nepanikar/app/theme/theme.dart';
 import 'package:nepanikar/l10n/ext.dart';
 import 'package:nepanikar/providers/localization_provider.dart';
 import 'package:nepanikar/router/routes.dart';
+import 'package:nepanikar/widgets/nepanikar_button.dart';
 import 'package:provider/provider.dart';
 
 class PlaygroundScreen extends StatelessWidget {
@@ -26,18 +28,6 @@ class PlaygroundScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              OutlinedButton(
-                onPressed: () {
-                  context.push(const AboutAppRoute().location);
-                },
-                child: Text(
-                  context.l10n.about_app,
-                ),
-              ),
-              Row(
-                children:
-                    _colors.map((e) => Flexible(child: Container(color: e, height: 30))).toList(),
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -64,11 +54,84 @@ class PlaygroundScreen extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 20),
+              ..._buildButtonsSection(context),
+              const SizedBox(height: 25),
+              _buildColorsSection(),
+              const SizedBox(height: 20),
               Text(context.l10n.depression_tips),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  List<Widget> _buildButtonsSection(BuildContext context) {
+    return [
+      Row(
+        children: [
+          Flexible(
+            child: NepanikarButton(
+              onTap: () => debugPrint('helloo'),
+              text: 'helloo',
+            ),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            child: NepanikarButton.secondary(
+              onTap: () => context.push(const AboutAppRoute().location),
+              text: context.l10n.about_app,
+            ),
+          ),
+        ],
+      ),
+      const SizedBox(height: 10),
+      Row(
+        children: [
+          Flexible(
+            child: NepanikarButton.async(
+              onTapAsync: () async => Future.delayed(const Duration(seconds: 2)),
+              expandToContentWidth: true,
+              trailingIcon: Assets.icons.navigation.right,
+              text: 'Spinner test',
+            ),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            child: NepanikarButton.secondaryAsync(
+              onTapAsync: () async => Future.delayed(const Duration(seconds: 2)),
+              expandToContentWidth: true,
+              text: 'Spinner test secondary',
+            ),
+          ),
+        ],
+      ),
+      const SizedBox(height: 10),
+      Row(
+        children: [
+          Flexible(
+            child: NepanikarButton(
+              onTap: () => debugPrint('disabled primary'),
+              enabled: false,
+              text: 'disabled primary',
+            ),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            child: NepanikarButton.secondary(
+              onTap: () => debugPrint('disabled secondary'),
+              enabled: false,
+              text: 'disabled secondary',
+            ),
+          ),
+        ],
+      ),
+    ];
+  }
+
+  Widget _buildColorsSection() {
+    return Row(
+      children: _colors.map((e) => Flexible(child: Container(color: e, height: 30))).toList(),
     );
   }
 }
