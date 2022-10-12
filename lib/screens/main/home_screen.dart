86394521fe4f91b00cc62a8fad844bbae76992ee
Martin Macek <martin.macek@blackpoint.dev>
2022-10-12 20:49:12 +0100

    feat: padding

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -90,7 +90,7 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 48),
               sliver: SliverGrid(
                 gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
                   maxCrossAxisExtent: 300.0,
