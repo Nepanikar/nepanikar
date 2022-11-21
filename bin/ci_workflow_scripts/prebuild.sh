b9ee4432d684a90d54caca94f15a8c8a1a75590a
Matěj Žídek <zidek.matt@gmail.com>
2022-11-21 21:52:40 +0100

    feat(ci): update workflow to generate 2 apks (#62)
    feat(ci): update workflow to generate 2 apks

--- bin/ci_workflow_scripts/prebuild.sh
+++ bin/ci_workflow_scripts/prebuild.sh
@@ -1,3 +1,3 @@
-sed -i -e "s#android:label=\"Nepanikař\"#android:label=\"Nepanikař v2\"#" android/app/src/main/AndroidManifest.xml
-sed -i -e "s#title: 'Nepanikař'#title: 'Nepanikař v2'" lib/main.dart
-sed -i -e "s#version: 0.0.2#version: 0.0.$1#" pubspec.yaml
+sed -i -e "s#android:label=\"Nepanikař\"#android:label=\"Nepanikař [dev - migration]\"#" android/app/src/main/AndroidManifest.xml
+sed -i -e "s#title: 'Nepanikař'#title: 'Nepanikař [dev - migration]'" lib/main.dart
+sed -i -e "s#version: 0.1.0#version: 0.1.$1#" pubspec.yaml
---
+++ bin/ci_workflow_scripts/prebuild_v2.sh
@@ -0,0 +1,9 @@
+# This generates .apk with different applicationId so that it can be
+# installed without overriding the original app.
+
+sed -i -e "s#android:label=\"Nepanikař\"#android:label=\"Nepanikař v2 [dev]\"#" android/app/src/main/AndroidManifest.xml
+sed -i -e "s#applicationId \"org.dontpanic\"#applicationId \"org.dontpanic.dev\"#" android/app/build.gradle
+sed -i -e "s#namespace \"org.dontpanic\"#namespace \"org.dontpanic.dev\"#" android/app/build.gradle
+sed -i -e "s#package org.dontpanic#package org.dontpanic.dev#" android/app/src/main/kotlin/org/dontpanic/MainActivity.kt
+sed -i -e "s#title: 'Nepanikař'#title: 'Nepanikař v2 [dev]'" lib/main.dart
+sed -i -e "s#version: 0.1.0#version: 0.1.$1#" pubspec.yaml
