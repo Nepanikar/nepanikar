f52a4ab383e142efc447747ade74e5823f90d1b6
Matej Zidek <zidek.matt@gmail.com>
2023-01-03 22:14:22 +0100

    fix if index less than 0 on elementAtOrNull

--- packages/nepanikar_data_migration/lib/src/parser/iterable_helpers.dart
+++ packages/nepanikar_data_migration/lib/src/parser/iterable_helpers.dart
@@ -3,9 +3,9 @@ import 'package:collection/collection.dart';
 // TODO: Taken from https://github.com/dart-lang/collection/pull/217 but can not use yet due to
 // dependency conflict.
 extension IterableExtension<T> on Iterable<T> {
-  T? elementAtOrNull(int index) => skip(index).firstOrNull;
+  T? elementAtOrNull(int index) => index <= -1 ? null : skip(index).firstOrNull;
 }
 
 extension ListExtensions<E> on List<E> {
-  E? elementAtOrNull(int index) => (index < length) ? this[index] : null;
+  E? elementAtOrNull(int index) => index <= -1 ? null : (index < length) ? this[index] : null;
 }
