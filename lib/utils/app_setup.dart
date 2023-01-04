b90daab789269a304caf55b3642a6e0c63d70359
Matej Zidek <zidek.matt@gmail.com>
2023-01-04 21:05:18 +0100

    feat: Firebase integration (Crashlytics)

--- lib/utils/app_setup.dart
+++ lib/utils/app_setup.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 import 'dart:ui' as ui;
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nepanikar/app/router/go_router_config.dart';
+import 'package:nepanikar/firebase_options.dart';
 import 'package:nepanikar/services/db/database_service.dart';
 import 'package:nepanikar/services/save_directories.dart';
 import 'package:nepanikar/utils/app_config.dart';
@@ -16,6 +20,20 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 Future<void> setup() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled
+  // by the Flutter framework to Crashlytics.
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
 
   // Initialize intl localizations.
   for (final delegate in AppLocalizations.localizationsDelegates) {
