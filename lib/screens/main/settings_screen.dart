8b485d7d81ab539ea1316abf42772e3d046046db
phiviet01 <duc.phiviet01@gmail.com>
2023-01-07 14:55:56 +0100

    Feat: Rate us (#104)
    feat(NEP-190): Open the app store after clicking on Rate us

--- lib/screens/main/settings_screen.dart
+++ lib/screens/main/settings_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nepanikar/app/generated/assets.gen.dart';
@@ -6,6 +8,7 @@ import 'package:nepanikar/app/router/routes.dart';
 import 'package:nepanikar/app/theme/fonts.dart';
 import 'package:nepanikar/screens/settings/export_screen.dart';
 import 'package:nepanikar/services/db/database_service.dart';
+import 'package:nepanikar/utils/app_config.dart';
 import 'package:nepanikar/utils/extensions.dart';
 import 'package:nepanikar/utils/registry.dart';
 import 'package:nepanikar/widgets/nepanikar_screen_wrapper.dart';
@@ -13,6 +16,8 @@ import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
+
+  AppConfig get _appConfig => registry.get<AppConfig>();
 
   DatabaseService get _databaseService => registry.get<DatabaseService>();
 
@@ -55,7 +60,16 @@ class SettingsScreen extends StatelessWidget {
                 _SettingsMenuItem(
                   leading: const Icon(Icons.shield_outlined),
                   text: context.l10n.rate,
-                  onTap: () {},
+                  onTap: () async {
+                    final uri = Uri.parse(
+                      Platform.isAndroid
+                          ? 'market://details?id=${_appConfig.googlePlayAppId}'
+                          : 'https://apps.apple.com/app/id${_appConfig.appStoreAppId}',
+                    );
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    }
+                  },
                 ),
                 _SettingsMenuItem(
                   leading: const Icon(Icons.shield_outlined),
