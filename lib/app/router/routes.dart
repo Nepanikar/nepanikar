52dd39df19a1ac1faf42eff6e7a6f301a5fd6558
phiviet01 <duc.phiviet01@gmail.com>
2023-01-08 13:27:14 +0100

    feat(NEP-80): Sponsor screen (#110)
    * feat(NEP-80): Sponsor screen
    
    * updated l10n
    
    Co-authored-by: Matej Zidek <zidek.matt@gmail.com>

--- lib/app/router/routes.dart
+++ lib/app/router/routes.dart
@@ -54,6 +54,7 @@ import 'package:nepanikar/screens/home/suicidal_thoughts/suicidal_thoughts_scree
 import 'package:nepanikar/screens/main/contacts_screen.dart';
 import 'package:nepanikar/screens/main/main_screen.dart';
 import 'package:nepanikar/screens/settings/export_screen.dart';
+import 'package:nepanikar/screens/settings/sponsors_screen.dart';
 
 part 'routes.g.dart';
 
@@ -87,6 +88,7 @@ class MainRoute extends GoRouteData {
 }
 
 const _settingsRoutes = <TypedGoRoute<GoRouteData>>[
+  TypedGoRoute<SponsorsRoute>(path: 'settings/sponsors'),
   TypedGoRoute<AboutAppRoute>(path: 'settings/about-app'),
   TypedGoRoute<ExportRoute>(path: 'settings/export'),
 ];
